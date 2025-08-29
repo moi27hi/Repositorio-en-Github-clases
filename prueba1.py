fb124e33bf38e37b8afe269b7c920281c42b5545
Moises Ramirez <abelinomoises@gmail.com>
2025-08-29 08:31:38 -0600

    Agregando cambios a prueba1

---
+++ prueba1.py
@@ -0,0 +1,6 @@
+
+
+nombre = input('Escribe tu nombre')
+
+
+print(f'Bienvenido{nombre}. Que tengas un buen dia')
---
+++ prueba2.py
@@ -0,0 +1,9 @@
+### Esto es una prueba.
+
+
+num1= int(input('Ingresa un numero '))
+num2 = int(input('Ingresa un numero '))
+
+
+suma = num1+num2 
+print(f"El resultado de la suma es {suma}")
