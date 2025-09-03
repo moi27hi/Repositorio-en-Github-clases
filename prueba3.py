195543b18df5775b2e0d32113a7af97a57eb3e72
MoisesRamirez-ugb <abelinomoises@gmail.com>
2025-09-03 12:53:53 -0600

    creacion de variables multiplicacion, division.

--- prueba3.py
+++ prueba3.py
@@ -6,5 +6,11 @@ num2 = int(input("Escribe un numero "))
 
 
 suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 /num2
 
 print(f'El resultado de la suma es {suma}')
+print(f'El resultado de la resta es {resta}')
+print(f'El resultado de la multiplicacion es {multiplicacion}')
+print(f'El resultado de la division es {division}')
