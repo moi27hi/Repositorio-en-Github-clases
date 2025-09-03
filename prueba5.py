11304092d062e720880313e1fb000279aaa65808
MoisesRamirez-ugb <abelinomoises@gmail.com>
2025-09-03 15:05:03 -0600

    Creacion de prueba5

---
+++ prueba5.py
@@ -0,0 +1,14 @@
+print('Hola mundo')
+
+num1 = int(input('Escribe un numero del uno al 10'))
+num2 = int(input('Escribe otro numero del 1 al 10'))
+
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
+print(f'''El resultado de la suma es {suma}
+El resultado de la resta es {resta}.
+El resultado de la multiplicacion es {multiplicacion}.
+El resultado de la division es {division} ''')
